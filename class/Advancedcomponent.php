dee431e476fb14baa1c9be4e83eec0f91f472feb
alee <zq@alee2002.com>
2011-11-15 21:52:20 +0800

    build extension

---
+++ class/Advancedcomponent.php
@@ -0,0 +1,15 @@
+<?php 
+namespace org\opencomb\advcmpnt ;
+
+use oc\ext\Extension ;
+
+class Advancedcomponent extends Extension 
+{
+	/**
+	 * 载入扩展
+	 */
+	public function load()
+	{
+		// todo ...
+	}
+}

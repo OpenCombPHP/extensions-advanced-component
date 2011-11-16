79782a7bdcb91b1b03bc3883b307945371f061b7
alee <zq@alee2002.com>
2011-11-16 10:23:37 +0800

    rename ext class

---
+++ class/AdvancedComponent.php
@@ -0,0 +1,15 @@
+<?php 
+namespace org\opencomb\advcmpnt ;
+
+use oc\ext\Extension ;
+
+class AdvancedComponent extends Extension 
+{
+	/**
+	 * 载入扩展
+	 */
+	public function load()
+	{
+		// todo ...
+	}
+}

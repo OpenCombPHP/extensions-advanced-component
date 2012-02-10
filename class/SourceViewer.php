a9514a1b26084c80bf9b6f388b44d1df7fc2acb5
kongyuan <anubiskong@gmail.com>
2012-02-10 19:44:05 +0800

    ...

--- class/SourceViewer.php
+++ class/SourceViewer.php
@@ -11,6 +11,15 @@ use org\opencomb\coresystem\auth\Id;
 use org\opencomb\doccenter\frame\DocFrontController;
 
 class SourceViewer extends DocFrontController {
+	/**
+	 * @wiki 高级工具/代码查看器
+	 * 
+	 * 代码生成器需要一些参数才能找到对应的文件
+	 * path  所在文件
+	 * 找到文件后,代码查看器会读取其中的内容然后用代码高亮插件显示出来
+	 * 可以以下参数来提高可读性:
+	 * hline 高亮行,单行用','号隔开,多行用'~'表示区间,例如 '2,4,34~44,55'
+	 */
 	public function createBeanConfig() {
 		return array (
 			'title' => '代码查看', 
@@ -31,26 +40,23 @@ class SourceViewer extends DocFrontController {
 	public function process() {
 		$this->checkPermissions('您没有使用这个功能的权限,无法继续浏览',array()) ;
 		
-		if(!$sSourceClass = $this->params['sclass']){
+		if(!$this->params->has('path')){
 			$this->messageQueue ()->create ( Message::error, "无法定位到指定代码,缺少信息" );
 		}
-		if(!$sSourcePackageNamespace = $this->params['spnamespace']){
-			$this->messageQueue ()->create ( Message::error, "无法定位到指定代码,缺少信息" );
-		}
-		if(!$sExtension = $this->params['extension']){
-			$this->messageQueue ()->create ( Message::error, "无法定位到指定代码,缺少信息" );
-		}
+		
 		if($this->params->has('hline')){
-			$this->viewViewer->variables ()->set ( 'sLines', str_replace('_', ',', $this->params->get('hline')) );
-		}
-		foreach(Extension::flyweight($sExtension)->metainfo()->packageIterator() as $arrPackage){
-			list($sNamespace,$sPackagePath) = $arrPackage ;
-			if($sNamespace == $sSourcePackageNamespace){
-				$sClassPath = Extension::flyweight($sExtension)->metainfo()->installPath().$sPackagePath . str_replace( '\\','/', substr($sSourceClass , strLen($sSourcePackageNamespace)) ).'.php';
-				$aSourceFile = FileSystem::singleton()->findFile($sClassPath);
-				break;
+			$arrLinesString = explode(',', $this->params->get('hline'));
+			for($i=0;$i<count($arrLinesString);$i++){
+				if(strpos($arrLinesString[$i] , '~')){
+					list($sStart , $sEnd) = explode('~',$arrLinesString[$i]);
+					$arrLinesString = array_merge($arrLinesString ,range((int)$sStart,(int)$sEnd) );
+				}
 			}
+			
+			$this->viewViewer->variables ()->set ( 'sLines', implode(',', $arrLinesString) );
 		}
+		$sClassPath = $this->params->get('path'); 
+		$aSourceFile = FileSystem::singleton()->findFile($sClassPath);
 		/**
 		 * @example /文件/文件内容读取
 		 * @forwiki /文件/文件内容读取
